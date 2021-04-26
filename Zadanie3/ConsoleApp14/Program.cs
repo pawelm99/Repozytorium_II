494cfd96f62cad1c7235c910d7b97991fefde384
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2021-04-26 20:31:55 +0200

    Zadanie3

---
+++ Zadanie3/ConsoleApp14/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Configuration;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var kontekst = new DBKontekst();
+            kontekst.Database.EnsureCreated();
+
+            kontekst.Users.Add(new MyUsers { Name = "Tomek2", RegistrationDate = DateTime.Today, Telefon = "513763123"});
+
+            kontekst.SaveChanges();
+
+            
+        }
+    }
+}
