5b3ba4c68b9006a1e2b412a683bb24bc26652c84
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2021-04-28 20:42:09 +0200

    Zadanie4

---
+++ Zadanie4/ConsoleApp19/MyContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseSqlServer(new Microsoft.Data.SqlClient.SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Nowa_baza3;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"));
+            base.OnConfiguring(dbContextOptionsBuilder);
+        }
+
+     
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+    }
+
+    public class Student : Person
+    {
+        public DateTime EnrollmentDate { get; set; }
+    }
+
+    public class Teacher : Person
+    {
+        public DateTime HireDate { get; set; }
+    }
+}
